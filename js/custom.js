3496b0b6a164acfb273036d8df0c6c42367394e5
Nahid islam <nahid789096@gmail.com>
2021-11-12 17:52:50 +0600

    file-add

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$(window).scroll(function(){
+    var scrolling = $(this).scrollTop();
+    if(scrolling > 50){
+        $(".navbar").addClass("bg");
+    }else{
+        $(".navbar").removeClass("bg");
+    }
+});
+var typed =new Typed('.type', {
+    
+    strings: ['Desginer','Professional Coder','Developer'],
+    loop: true,
+    typeSpeed: 0,
+    backSpeed: 0,
+    smartBackspace: true,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true,
+
+});
+$(".offcanvas ul li a").click(function(){
+    $(".offcanvas").fadeOut();
+})
+$(".navbar-toggler").click(function(){
+    $(".offcanvas").fadeIn();
+})
